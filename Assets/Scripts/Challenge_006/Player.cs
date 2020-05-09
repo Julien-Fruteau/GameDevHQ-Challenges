2108793e1f0f97a91503f3525410f1142ee91aab
Julien-Fruteau <julien.fruteau@gmail.com>
2020-05-09 18:03:45 +0200

    Add defaut gray mat, Player input Action and init condition for player

---
+++ Assets/Scripts/Challenge_006/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Challenge_006
+{
+    public class Player : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        Rigidbody _rb;
+
+        MeshRenderer _meshRenderer;
+
+        // Hint that tricked me in UI, drag script from gameObject to event and not from script asset folder
+        // so that event discover method
+        public void OnAction(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _rb.useGravity = true;
+            }
+        }
+
+        void Start()
+        {
+            // Get Rigidbody and remove gravity so that is does NOT fall
+            _rb = gameObject.GetComponent<Rigidbody>();
+            if (_rb == null)
+            {
+                _rb = gameObject.AddComponent<Rigidbody>();
+            }
+            _rb.useGravity = false;
+
+            // Get MeshRenderer to Access Color
+            _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+            // _meshRenderer.material.color = Color.blue;
+
+            // tag player because we will use the tag to identify the Player when it will hit the floor
+            gameObject.tag = "Player";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
